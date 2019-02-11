f3a4fbb63e665390f00c398c227929b59f80c929
Olivier Champion <ochampion@me.com>
2019-02-11 16:41:26 +0100

    Add project files.

---
+++ CSharp8News/CSharp8News/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CSharp8News
+{
+s
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello World!");
+    }
+}
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            double d;
+            if ((d=getNumber())==3.5)
+            {
+                Console.WriteLine("yes");
+            }
+
+            Console.ReadKey();
+        }
+
+        private static double getNumber()
+        {
+            return 3.5;
+        }
+    }
+}
